535c106f30f064822def9aaa7002ddc4170e9b04
Huascar <huascarmc@hotmail.com>
2018-04-15 20:47:06 +0100

    added test NotEnoughChange message PASSED

--- src/test/java/com.example.vendingMachine.api/vendingMachine/Exceptions/TestNotEnoughChangeException.java
+++ src/test/java/com.example.vendingMachine.api/vendingMachine/Exceptions/TestNotEnoughChangeException.java
@@ -3,6 +3,8 @@ package com.example.vendingmachine.api;
 import vendingMachine.exceptions.NotEnoughChangeException;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class TestNotEnoughChangeException {
 
@@ -10,4 +12,14 @@ public class TestNotEnoughChangeException {
     public void testNotEnoughChangeException() {
         throw new NotEnoughChangeException();
     }
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void testNotEnoughChangeMessage() throws Exception {
+      expectedEx.expect(RuntimeException.class);
+      expectedEx.expectMessage("NotEnoughChange");
+      throw new NotEnoughChangeException();
+  }
 }
