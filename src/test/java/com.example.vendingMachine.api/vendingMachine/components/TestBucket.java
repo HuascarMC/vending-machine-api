bc4df49d803c5027b06c7c66caa2c119da4e7bee
Huascar <huascarmc@hotmail.com>
2018-04-15 20:58:01 +0100

    created test bucket file?

---
+++ src/test/java/com.example.vendingMachine.api/vendingMachine/components/TestBucket.java
@@ -0,0 +1,13 @@
+package com.example.vendingmachine.api;
+
+import vendingMachine.exceptions.components.Bucket;
+import vendingMachine.exceptions.Objects.Item;
+import vendingMachine.exceptions.Objects.Coin;
+import vendingMachine.exceptions.Objects.State;
+
+import org.junit.Before;
+
+public class TestBucket {
+
+
+}
