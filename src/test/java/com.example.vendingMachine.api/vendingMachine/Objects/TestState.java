d655e81005d24d695d80a2064d09c03bb0228ac4
Huascar <huascarmc@hotmail.com>
2018-04-15 21:02:56 +0100

    created test state file

---
+++ src/test/java/com.example.vendingMachine.api/vendingMachine/Objects/TestState.java
@@ -0,0 +1,16 @@
+package com.example.vendingmachine.api;
+
+import vendingMachine.Objects.State;
+import vendingMachine.exceptions.NotEnoughBalanceException;
+import vendingMachine.exceptions.OutOfStockException;
+import vendingMachine.exceptions.NotEnoughChangeException;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TestState {
+
+
+}
