7ff0d053ca6dc0796b36bd505ce6e084db71176a
Huascar <huascarmc@hotmail.com>
2018-04-15 17:39:25 +0100

    manual test on routes passed

--- src/main/java/com.example.vendingMachine.api/api/App.java
+++ src/main/java/com.example.vendingMachine.api/api/App.java
@@ -2,6 +2,29 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+    get("/", (request, response) -> {
+      // Show something
+      return "";
+    });
+
+    post("/", (request, response) -> {
+      // Create something
+      return "";
+    });
+
+    put("/", (request, response) -> {
+      // Update something
+      return "";
+    });
+
+    delete("/", (request, response) -> {
+    // Annihilate something
+    return "";
+    });
+
+    options("/", (request, response) -> {
+    // Appease something
+    return "";
+    });
+  }
 }
