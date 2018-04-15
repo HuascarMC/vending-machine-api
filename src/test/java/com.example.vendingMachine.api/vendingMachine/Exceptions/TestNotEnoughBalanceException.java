80a0f4a6b1c60b7dbdf148efbed4c15ba47ea9ca
Huascar <huascarmc@hotmail.com>
2018-04-15 20:45:16 +0100

    added test NotEnoughBalanceMessage

--- src/test/java/com.example.vendingMachine.api/vendingMachine/Exceptions/TestNotEnoughBalanceException.java
+++ src/test/java/com.example.vendingMachine.api/vendingMachine/Exceptions/TestNotEnoughBalanceException.java
@@ -3,6 +3,8 @@ package com.example.vendingmachine.api;
 import vendingMachine.exceptions.NotEnoughBalanceException;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class TestNotEnoughBalanceException {
 
@@ -10,4 +12,14 @@ public class TestNotEnoughBalanceException {
     public void testNotEnoughBalanceException() {
         throw new NotEnoughBalanceException();
     }
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void testNotEnoughBalanceMessage() throws Exception {
+      expectedEx.expect(RuntimeException.class);
+      expectedEx.expectMessage("NotEnoughBalance");
+      throw new NotEnoughBalanceException();
+  }
 }
