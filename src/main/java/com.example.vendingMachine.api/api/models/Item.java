07946fb54a7d6b9b731b63d289ae9863edf078ed
Huascar <huascarmc@hotmail.com>
2018-04-17 08:55:26 +0100

    added oneToMany annot item

--- src/main/java/com.example.vendingMachine.api/api/models/Item.java
+++ src/main/java/com.example.vendingMachine.api/api/models/Item.java
@@ -3,19 +3,29 @@ package api.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 
 @Entity
-@Table(name="item", schema = "public")
+@Table(name="item")
 public class Item {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue
     int id;
+
+    @Column(name = "name")
     String name;
+
+    @Column(name = "quantity")
+    @OneToMany(mappedBy = "name")
     Integer quantity;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
         return id;
     }
