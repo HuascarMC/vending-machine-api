39f883fdca11cc345b24f8eb4320c748268fc327
Huascar <huascarmc@hotmail.com>
2018-04-19 15:10:07 +0100

    codebase refactored to integrate db with routes complete

--- src/main/java/com.example.vendingMachine.api/api/App.java
+++ src/main/java/com.example.vendingMachine.api/api/App.java
@@ -87,98 +87,10 @@ public class App {
       return gson.toJson("OK");
     });
 
-    // });
-
-  //   // Update coin
-  //   put("/machine/DBCoin", (request, response) -> {
-  //     EntityManager session = sf.createEntityManager();
-  //     try{
-  //       Gson gson = new Gson();
-  //
-  //       String query = String.format("FROM DBCoin WHERE name = '%s'", request.queryParams("name"));
-  //       DBCoin coin = (DBCoin) session.createQuery(query).getSingleResult();
-  //       coin.setQuantity(Integer.parseInt(request.queryParams("quantity")));
-  //       session.getTransaction().begin();
-  //       session.merge(coin);
-  //       session.getTransaction().commit();
-  //       return gson.toJson(coin);
-  //     } catch(Exception e) {
-  //       return "Error: " + e.getMessage();
-  //     } finally {
-  //       if(session.isOpen()) {
-  //         session.close();
-  //       }
-  //     }
-  //   });
-  //
-  //   // Delete all items
-  //   delete("/machine/:inventory", (request, response) -> {
-  //     Gson gson = new Gson();
-  //     EntityManager session = sf.createEntityManager();
-  //     try {
-  //       request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement(""));
-  //       session.getTransaction().begin();
-  //       session.createQuery("DELETE FROM " + request.params(":inventory")).executeUpdate();
-  //       session.getTransaction().commit();
-  //       return "OK";
-  //     } catch (Exception e) {
-  //       return "Error: " + e.getMessage();
-  //     } finally {
-  //       if (session.isOpen()) {
-  //         session.close();
-  //       }
-  //     }
-  //   });
-  //   // Create item
-  //   post("/machine/DBItem", (request, response) -> {
-  //     Gson gson = new Gson();
-  //     EntityManager session = sf.createEntityManager();
-  //     try {
-  //       request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement(""));
-  //
-  //       DBItem item = (DBItem) gson.fromJson(request.body(), DBItem.class);
-  //
-  //       session.getTransaction().begin();
-  //       session.persist(item);
-  //       session.getTransaction().commit();
-  //
-  //       return gson.toJson(item);
-  //     } catch (Exception e) {
-  //       return "Error: " + e.getMessage();
-  //     } finally {
-  //       if (session.isOpen()) {
-  //         session.close();
-  //       }
-  //
-  //     }
-  //   });
-  //
-  //   put("/machine/DBItem", (request, response) -> {
-  //     // Update item
-  //     EntityManager session = sf.createEntityManager();
-  //     try{
-  //       Gson gson = new Gson();
-  //
-  //       String query = String.format("FROM DBItem WHERE name = '%s'", request.queryParams("name"));
-  //       DBItem item = (DBItem) session.createQuery(query).getSingleResult();
-  //       item.setQuantity(Integer.parseInt(request.queryParams("quantity")));
-  //       session.getTransaction().begin();
-  //       session.merge(item);
-  //       session.getTransaction().commit();
-  //       return gson.toJson(item);
-  //     } catch(Exception e) {
-  //       return "Error: " + e.getMessage();
-  //     } finally {
-  //       if(session.isOpen()) {
-  //         session.close();
-  //       }
-  //     }
-  //   });
-  //
-  //
-  //   options("/options", (request, response) -> {
-  //     // Appease something
-  //     return "options";
-  //   });
+    options("/options", (request, response) -> {
+      // Appease something
+      Gson gson = new Gson();
+      return gson.toJson("options");
+    });
   }
 }
