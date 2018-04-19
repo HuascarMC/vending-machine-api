6585c865cc453a73c0932cc3bc6bf2080239c2a4
Huascar <huascarmc@hotmail.com>
2018-04-19 09:32:56 +0100

    dbhelper + removeOneItem method

--- src/main/java/com.example.vendingMachine.api/api/db/DBHelper.java
+++ src/main/java/com.example.vendingMachine.api/api/db/DBHelper.java
@@ -18,18 +18,28 @@ public class DBHelper {
   }
 
   public double getCoinTotal(String params) {
-       double result = 0.00;
-       List<Coin> coins = session.createQuery("FROM " + params).getResultList();
-       for(Coin coin : coins) {
-         result += coin.getValue();
-       }
-       return result;
+    double result = 0.00;
+    List<Coin> coins = session.createQuery("FROM " + params).getResultList();
+    for(Coin coin : coins) {
+      result += coin.getValue();
+    }
+    return result;
   }
 
   public boolean hasItem(String itemName) {
     String query = String.format("FROM DBCoin WHERE name = '%s'", itemName);
     DBItem item = (DBItem) session.createQuery(query).getSingleResult();
     return (item.getQuantity() > 0);
+  }
+
+  public void removeOneItem(String itemName) {
+    String query = String.format("FROM DBItem WHERE name = '%s'", itemName);
+    DBItem item = (DBItem) session.createQuery(query).getSingleResult();
+    Integer currentQuantity = item.getQuantity();
+    item.setQuantity(currentQuantity - 1);
+    session.getTransaction().begin();
+    session.merge(item);
+    session.getTransaction().commit();
   }
 
 }
