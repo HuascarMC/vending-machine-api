9cafbefe82ca9bcd18b617a4a06f108248747bdf
Huascar <huascarmc@hotmail.com>
2018-04-19 09:28:07 +0100

    added method hasitem? dbhelper

--- src/main/java/com.example.vendingMachine.api/api/db/DBHelper.java
+++ src/main/java/com.example.vendingMachine.api/api/db/DBHelper.java
@@ -3,22 +3,33 @@ package api.models;
 import javax.persistence.EntityManager;
 import org.hibernate.SessionFactory;
 import vendingMachine.Objects.Coin;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 public class DBHelper {
 
+  private SessionFactory sf;
+  private EntityManager session;
 
   public void DBHelper() {
     SessionFactory sf = new Configuration().configure().buildSessionFactory();
     EntityManager session = sf.createEntityManager();
   }
 
-  public double getCoinTotal() {
+  public double getCoinTotal(String params) {
        double result = 0.00;
-       List<Coin> coins = session.createQuery("FROM " + request.params("DBCoin")).getResultList();
+       List<Coin> coins = session.createQuery("FROM " + params).getResultList();
        for(Coin coin : coins) {
-         result += coin.value;¿
+         result += coin.getValue();
        }
        return result;
+  }
+
+  public boolean hasItem(String itemName) {
+    String query = String.format("FROM DBCoin WHERE name = '%s'", itemName);
+    DBItem item = (DBItem) session.createQuery(query).getSingleResult();
+    return (item.getQuantity() > 0);
   }
 
 }
